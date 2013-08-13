77be90908c1f1af4254770f06ffd075f74ca3c62
Eneko Illarramendi <eneko@illarra.com>
2013-08-13 12:30:15 +0200

    Secure user management area in Admin

--- Controller/Admin/UserController.php
+++ Controller/Admin/UserController.php
@@ -6,6 +6,7 @@ use Illarra\CoreBundle\Controller\Admin\BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 /**
  * @Route("/user")
@@ -27,6 +28,7 @@ class UserController extends BaseController
     }
 
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/{page}", name="admin_illarra_user_index", defaults={"page" = 1}, requirements={"page" = "\d+"})
      * @Method("GET")
      */
@@ -36,6 +38,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/{id}/show", name="admin_illarra_user_show")
      * @Method("GET")
      */
@@ -45,6 +48,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/create", name="admin_illarra_user_create")
      * @Method("POST")
      */
@@ -54,6 +58,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/new", name="admin_illarra_user_new")
      * @Method("GET")
      */
@@ -63,6 +68,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/{id}/edit", name="admin_illarra_user_edit")
      * @Method("GET")
      */
@@ -72,6 +78,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/{id}/update", name="admin_illarra_user_update")
      * @Method("PUT")
      */
@@ -81,6 +88,7 @@ class UserController extends BaseController
     }
     
     /**
+     * @Secure(roles="ROLE_ADMIN_USERS")
      * @Route("/{id}/delete", name="admin_illarra_user_delete")
      * @Method("DELETE")
      */
